0b4be6d56b96b7f80ab9f249bb54aa7bc05ff26a
leogracas34-gif <leogracas34@gmail.com>
2026-02-28 21:01:27 -0300

    Update LoginActivity.kt

--- app/src/main/java/com/vltv/play/LoginActivity.kt
+++ app/src/main/java/com/vltv/play/LoginActivity.kt
@@ -280,7 +280,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun abrirHomeDireto() {
-        val intent = Intent(this, ProfilesActivity::class.java)
+        val intent = Intent(this, HomeActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
         finish()
